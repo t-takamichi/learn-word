2f6a6fce98301debebaacf9a5a4834d0e73355c5
t-takamichi <takamichitanaka10@gmail.com>
2024-02-18 14:45:19 +0900

    update:テスト用データの追加

--- services/get_word_servce.go
+++ services/get_word_servce.go
@@ -14,11 +14,18 @@ func NewGetWordService() GetWordService {
 
 func (*getWordService) GetWordAll(ctx context.Context) []Word {
 	words := []Word{}
-	words = append(words, Word{
-		Id:         "1",
-		Vocabulary: "learn",
-		Mean:       "稼ぐ",
-	})
+	words = append(words,
+		Word{
+			Id:         "1",
+			Vocabulary: "learn",
+			Mean:       "稼ぐ",
+		},
+		Word{
+			Id:         "2",
+			Vocabulary: "study",
+			Mean:       "勉強する",
+		},
+	)
 	return words
 }
 
